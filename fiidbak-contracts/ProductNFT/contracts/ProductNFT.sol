15a8b05a7d9f3cde573a55d73c7de23aa5dd5878
AbolareRoheemah <roheemahabo@gmail.com>
2025-08-20 00:02:58 +0100

    worked on product nft contract

--- fiidbak-contracts/ProductNFT/contracts/ProductNFT.sol
+++ fiidbak-contracts/ProductNFT/contracts/ProductNFT.sol
@@ -3,10 +3,14 @@
 pragma solidity ^0.8.27;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC1155, Ownable {
-    uint256 private _nextTokenId;
+contract MyToken is ERC1155, Ownable, AccessControl {
+    uint256 private _nextTokenId = 1;
+    bytes32 public constant VERIFIER_ROLE = keccak256("VERIFIER_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
     // product name, desc, and other info will be stored on IPFS
     struct Product {
         uint256 product_id;
@@ -14,32 +18,103 @@ contract MyToken is ERC1155, Ownable {
         bytes ipfs_cid;
     }
 
-    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+    constructor() ERC1155("") Ownable(msg.sender) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(VERIFIER_ROLE, msg.sender);
+    }
 
     event ProductMinted(address indexed product_owner, uint256 tokenId, uint256 amount);
+    event ProductDeleted(address indexed product_owner, uint256 tokenId, uint256 amount);
 
-    mapping (uint => Product) productbyId;
-    mapping (address => Product[]) ownerProducts;
-    mapping (address => mapping (uint => Product)) ownerProductByID;
-    mapping(address => mapping(uint => bool)) ownerProductExists;
+    mapping (uint256 => Product) public productsById;
+    mapping (address => uint256[]) public productIdsByOwner;
+    mapping(address => mapping(uint256 => bool)) public isOwnerOfProduct;
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mintProduct(address product_owner, uint256 token_id, bytes memory ipfs_cid)
+    function mintProduct(address product_owner, bytes memory ipfs_cid)
         public
     {
         require(product_owner != address(0), "invalid owner address");
         require(ipfs_cid.length != 0, "IPFS CID is required");
-        require(!ownerProductExists[product_owner][token_id], "product with same ID exists");
-        _mint(product_owner, token_id, 1, ipfs_cid);
+        uint256 tokenId = _nextTokenId++;
+        require(!isOwnerOfProduct[product_owner][tokenId], "user already owns this product");
+        
+        Product memory newProduct = Product({
+            product_id: tokenId,
+            product_owner: product_owner,
+            ipfs_cid: ipfs_cid
+        });
+        
+        productsById[tokenId] = newProduct;
+        productIdsByOwner[product_owner].push(tokenId);
+        isOwnerOfProduct[product_owner][tokenId] = true;
+        
+        _mint(product_owner, tokenId, 1, "");
+        _grantRole(CREATOR_ROLE, product_owner);
+
+        emit ProductMinted(product_owner, tokenId, 1);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
+    function deleteProduct(uint256 product_id) public onlyRole(CREATOR_ROLE) {
+        require(msg.sender != address(0), "Invalid address");
+        require(isOwnerOfProduct[msg.sender][product_id], "Cant delete other's product");
+        _burn(msg.sender, product_id, balanceOf(msg.sender, product_id));
+
+        emit ProductDeleted(msg.sender, product_id, 1);
+    }
+
+    function deleteProduct(address product_owner, uint256 product_id) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(product_owner != address(0), "Invalid address");
+        require(isOwnerOfProduct[product_owner][product_id], "Owner doesnt own the product");
+        _burn(product_owner, product_id, 1);
+
+        emit ProductDeleted(product_owner, product_id, 1);
+    }
+
+    function addVerifier(address verifier) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(VERIFIER_ROLE, verifier);
+    }
+
+    function removeVerifier(address verifier) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(VERIFIER_ROLE, verifier);
+    }
+
+    function verifyProof(bytes calldata proof, address user) internal pure returns (bool) {
+    // ECDSA recover for a signed message
+    // Assumes `proof` is the signature, and `user` is the expected signer.
+    // You must hash the message in the same way it was signed (e.g., using eth_sign or EIP-191).
+    // For demonstration, let's assume the message is simply the user's address.
+    bytes32 messageHash = keccak256(abi.encodePacked(user));
+    bytes32 ethSignedMessageHash = keccak256(
+        abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+    );
+
+    // Signature must be 65 bytes (r,s,v)
+    if (proof.length != 65) {
+        return false;
+    }
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+        r := calldataload(proof.offset)
+        s := calldataload(add(proof.offset, 32))
+        v := byte(0, calldataload(add(proof.offset, 64)))
+    }
+    // EIP-2 still allows only lower s values, and v should be 27 or 28
+    if (v < 27) v += 27;
+    if (v != 27 && v != 28) {
+        return false;
+    }
+    address recovered = ecrecover(ethSignedMessageHash, v, r, s);
+    return (recovered == user);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
