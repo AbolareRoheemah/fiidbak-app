d653111d0cc01317ab683545326a9c71c4b391c7
AbolareRoheemah <roheemahabo@gmail.com>
2025-08-20 22:53:03 +0100

    major functions in ProductNFT contract done

--- fiidbak-contracts/ProductNFT/contracts/ProductNFT.sol
+++ fiidbak-contracts/ProductNFT/contracts/ProductNFT.sol
@@ -15,7 +15,7 @@ contract MyToken is ERC1155, Ownable, AccessControl {
     struct Product {
         uint256 product_id;
         address product_owner;
-        bytes ipfs_cid;
+        string ipfs_cid;
     }
 
     constructor() ERC1155("") Ownable(msg.sender) {
@@ -25,22 +25,34 @@ contract MyToken is ERC1155, Ownable, AccessControl {
 
     event ProductMinted(address indexed product_owner, uint256 tokenId, uint256 amount);
     event ProductDeleted(address indexed product_owner, uint256 tokenId, uint256 amount);
+    event URISet(string newUri);
 
     mapping (uint256 => Product) public productsById;
-    mapping (address => uint256[]) public productIdsByOwner;
-    mapping(address => mapping(uint256 => bool)) public isOwnerOfProduct;
+    // mapping (address => uint256[]) public productIdsByOwner;
+    mapping(uint256 => string) private _tokenURIs;
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
+        emit URISet(newuri);
     }
 
-    function mintProduct(address product_owner, bytes memory ipfs_cid)
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        string memory tokenURI = _tokenURIs[tokenId];
+        if (bytes(tokenURI).length > 0) {
+            return tokenURI;
+        }
+        return super.uri(tokenId);
+    }
+
+    function mintProduct(address product_owner, uint256 amount, string memory ipfs_cid)
         public
     {
         require(product_owner != address(0), "invalid owner address");
-        require(ipfs_cid.length != 0, "IPFS CID is required");
+        require(bytes(ipfs_cid).length != 0, "IPFS CID is required");
+        require(amount > 0, "Amount must be positive");
+
         uint256 tokenId = _nextTokenId++;
-        require(!isOwnerOfProduct[product_owner][tokenId], "user already owns this product");
+        require(balanceOf(product_owner, tokenId) == 0, "user already owns this product");
         
         Product memory newProduct = Product({
             product_id: tokenId,
@@ -49,27 +61,33 @@ contract MyToken is ERC1155, Ownable, AccessControl {
         });
         
         productsById[tokenId] = newProduct;
-        productIdsByOwner[product_owner].push(tokenId);
-        isOwnerOfProduct[product_owner][tokenId] = true;
+        // productIdsByOwner[product_owner].push(tokenId);
+        _tokenURIs[tokenId] = string(abi.encodePacked("ipfs://", ipfs_cid));
         
-        _mint(product_owner, tokenId, 1, "");
+        _mint(product_owner, tokenId, amount, "");
         _grantRole(CREATOR_ROLE, product_owner);
 
-        emit ProductMinted(product_owner, tokenId, 1);
+        emit ProductMinted(product_owner, tokenId, amount);
     }
 
     function deleteProduct(uint256 product_id) public onlyRole(CREATOR_ROLE) {
         require(msg.sender != address(0), "Invalid address");
-        require(isOwnerOfProduct[msg.sender][product_id], "Cant delete other's product");
-        _burn(msg.sender, product_id, balanceOf(msg.sender, product_id));
+        require(balanceOf(msg.sender, product_id) > 0, "You dont own this product");
+        uint256 amt = balanceOf(msg.sender, product_id);
+        _burn(msg.sender, product_id, amt);
+        delete productsById[product_id];
+        delete _tokenURIs[product_id];
 
-        emit ProductDeleted(msg.sender, product_id, 1);
+        emit ProductDeleted(msg.sender, product_id, amt);
     }
 
-    function deleteProduct(address product_owner, uint256 product_id) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function deleteProductAdmin(address product_owner, uint256 product_id) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(product_owner != address(0), "Invalid address");
-        require(isOwnerOfProduct[product_owner][product_id], "Owner doesnt own the product");
-        _burn(product_owner, product_id, 1);
+        require(balanceOf(product_owner, product_id) > 0, "Owner doesn't own the product");
+        uint256 amt = balanceOf(msg.sender, product_id);
+        _burn(product_owner, product_id, amt);
+        delete productsById[product_id];
+        delete _tokenURIs[product_id];
 
         emit ProductDeleted(product_owner, product_id, 1);
     }
@@ -82,36 +100,80 @@ contract MyToken is ERC1155, Ownable, AccessControl {
         _revokeRole(VERIFIER_ROLE, verifier);
     }
 
-    function verifyProof(bytes calldata proof, address user) internal pure returns (bool) {
-    // ECDSA recover for a signed message
-    // Assumes `proof` is the signature, and `user` is the expected signer.
-    // You must hash the message in the same way it was signed (e.g., using eth_sign or EIP-191).
-    // For demonstration, let's assume the message is simply the user's address.
-    bytes32 messageHash = keccak256(abi.encodePacked(user));
-    bytes32 ethSignedMessageHash = keccak256(
-        abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
-    );
+    function getProduct(uint256 product_id) external view returns (Product memory) {
+        require(product_id <= _nextTokenId, "invalid product ID");
+        return productsById[product_id];
+    }
 
-    // Signature must be 65 bytes (r,s,v)
-    if (proof.length != 65) {
-        return false;
+    function getAllProducts(uint256 product_count, uint256 start_index) external view returns (Product[] memory) {
+        require(product_count > 0, "invalid count");
+        require(start_index < _nextTokenId, "start index higher than product length");
+
+        uint256 availableProducts = _nextTokenId - start_index;
+        uint256 returnCount = availableProducts < product_count ? availableProducts: product_count;
+        
+        Product[] memory products = new Product[](returnCount);
+
+        for (uint i = 0; i < returnCount; i++) {
+            uint256 productId = start_index + i;
+            if (productsById[productId].product_id != 0) {
+                products[i] = productsById[i];
+            }
+        }
+
+        return products;
     }
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-        r := calldataload(proof.offset)
-        s := calldataload(add(proof.offset, 32))
-        v := byte(0, calldataload(add(proof.offset, 64)))
+
+    function getOwnerProducts(address product_owner, uint256 product_count, uint256 start_index) external view returns (Product[] memory) {
+        require(product_owner != address(0), "invalid product owner address");
+        require(product_count > 0, "invalid count");
+        require(start_index < _nextTokenId, "start index higher than product length");
+
+        uint256 availableProducts = _nextTokenId - start_index;
+        uint256 returnCount = availableProducts < product_count ? availableProducts: product_count;
+
+        Product[] memory products = new Product[](returnCount);
+
+        for (uint256 i; i < returnCount; i++) {
+            uint256 productId = start_index + i;
+            if (balanceOf(product_owner, productId) > 0) {
+                products[i] = productsById[productId];
+            }
+        }
+
+        return products;
     }
-    // EIP-2 still allows only lower s values, and v should be 27 or 28
-    if (v < 27) v += 27;
-    if (v != 27 && v != 28) {
-        return false;
-    }
-    address recovered = ecrecover(ethSignedMessageHash, v, r, s);
-    return (recovered == user);
+
+    function verifyProof(bytes calldata proof, address user) internal pure returns (bool) {
+        // ECDSA recover for a signed message
+        // Assumes `proof` is the signature, and `user` is the expected signer.
+        // You must hash the message in the same way it was signed (e.g., using eth_sign or EIP-191).
+        // For demonstration, let's assume the message is simply the user's address.
+        bytes32 messageHash = keccak256(abi.encodePacked(user));
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+        );
+
+        // Signature must be 65 bytes (r,s,v)
+        if (proof.length != 65) {
+            return false;
+        }
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            r := calldataload(proof.offset)
+            s := calldataload(add(proof.offset, 32))
+            v := byte(0, calldataload(add(proof.offset, 64)))
+        }
+        // EIP-2 still allows only lower s values, and v should be 27 or 28
+        if (v < 27) v += 27;
+        if (v != 27 && v != 28) {
+            return false;
+        }
+        address recovered = ecrecover(ethSignedMessageHash, v, r, s);
+        return (recovered == user);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
